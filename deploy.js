525906a5a2250e121c70ee96997414189d118e2e
Terrence Harvey <ramarduk@gmail.com>
2019-04-30 12:22:10 -0600

    added node-key

--- deploy.js
+++ deploy.js
@@ -87,7 +87,7 @@ function sshConnect() {
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
       host: '52.23.201.23',
       username: 'ubuntu',
-      privateKey: 'hs-key.pem'
+      privateKey: 'node-key.pem'
     })
     .then(function() {
       console.log('SSH Connection established.');
